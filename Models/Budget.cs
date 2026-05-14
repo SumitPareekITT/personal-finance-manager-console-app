6a7c6b96e5d203e8d2d73055f8798913dc91d9fc
Sumit Pareek <sumit.pareek@intimetec.com>
2026-05-14 14:34:49 +0530

    Added entity models for finance management

--- Models/Budget.cs
+++ Models/Budget.cs
@@ -1,7 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PersonalFinanceManager.Models
 {
     public class Budget
     {
         public int Id { get; set; }
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+
+        [Required]
+        public decimal LimitAmount { get; set; }
     }
 }
